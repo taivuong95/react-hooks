a6a270b831598e3037faef542b397ffd5d3d12b6
Tai Vuong <taivt1195@gmail.com>
2021-01-12 17:10:58 +0700

    firest

---
+++ state-hook/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCounterThree from './components/HookCounterThree';
+import HookCounterFour from './components/HookCounterFour';
+import ClassCounterOne from './components/ClassCounterOne';
+import HookCounterOne from './components/HookCounterOne';
+import ClassMouse from './components/ClassMouse';
+import HookMouse from './components/HookMouse';
+import MouseContainer from './components/MouseContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <MouseContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ state-hook/src/components/HookMouse.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+
+function HookMouse() {
+    const [x, setX] = useState(0);
+    const [y, setY] = useState(0);
+
+    const logMousePosition = e => {
+        setX(e.clientX);
+        setY(e.clientY);
+    }
+    useEffect(() => {
+        window.addEventListener('mousemove', logMousePosition);
+
+        return () => {
+            window.removeEventListener('mousemove', logMousePosition);
+        }
+    }, [])
+
+    return (
+        <div>
+            X - {x} Y - {y}
+        </div>
+    );
+}
+
+export default HookMouse;
